4c1a63f367c668949ac239da730acec993c7248e
Tapas <Tapas.Kumar@simplyfi.tech>
2023-12-10 17:26:44 +0530

    Changes

--- 2.Pattern printing/printing pattern4/tempCodeRunnerFile.cpp
+++ 2.Pattern printing/printing pattern4/tempCodeRunnerFile.cpp
@@ -6,19 +6,15 @@ int main()
     int num;
     cout<<"Enter a number: ";
     cin>>num;
-    for(row=num;row>=1;row--)
+    for(row=1;row<=num;row++)
     {
         for(col=1;col<=num-row;col++)
         {
             cout<<"  ";
         }
-         for(col=1;col<=row;col++)
+         for(col=1;col<=2*row-1;col++)
         {
-            cout<<"*"<<" ";
-        }
-        for(col=row-1;col>=1;col--)
-        {
-            cout<<"*"<<" ";
+            cout<<"* ";
         }
         cout<<endl;
     }
