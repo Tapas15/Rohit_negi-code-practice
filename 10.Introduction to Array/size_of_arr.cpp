61c5885c7549f26e06f29d64ee071215cae0e41e
Tapas <Tapas.Kumar@simplyfi.tech>
2023-12-12 00:09:46 +0530

    Introduction to array

---
+++ 10.Introduction to Array/size_of_arr.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int arr[5] = {1,2,3,4,5};
+   cout<<sizeof(arr)<<" ";
+
+//    char arr[6] = {1,2,3,4,5,6};
+//    cout<<sizeof(arr)<<" ";
+    
+
+    return 0;
+}
---
+++ 10.Introduction to Array/userinput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[6];
+    cout<<"Enter 6 numbers: ";
+    for(int i=0;i<6;i++)
+    cin>>arr[i];
+    for(int i=0;i<6;i++)
+    //cout<<"Array :";
+    cout<<arr[i]<<" ";
+    cout<<endl<<"Array";
+
+    return 0;
+}
